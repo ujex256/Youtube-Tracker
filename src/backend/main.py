cef849a5aa8d2b097025124b409fc03edb7fcdbd
fffena <132272963+fffena@users.noreply.github.com>
2024-02-22 13:41:34 +0000

    add-endpoint: 登録するエンドポイント

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from dotenv import load_dotenv
+from os import getenv
 
+from models import input_schemas
+from db import RecordDeta
+
+load_dotenv()
 app = FastAPI()
+record = RecordDeta(youtube_api_key=getenv("YOUTUBE_TOKEN"))
 
 
 @app.get("/")
 async def test():
     return "Hello, world!"
+
+
+@app.post("/video/register")
+async def register(a: input_schemas.YTVideoInput):
+    record.register_video(video_id=a.video_id)
+    return JSONResponse({"status": True})
