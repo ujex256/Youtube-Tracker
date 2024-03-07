1859aeacf60016f4d0b7eaa7a6586137ef5ba13f
fffena <132272963+fffena@users.noreply.github.com>
2024-03-07 12:44:37 +0000

    fix: 現在のDB構造に修正

--- src/backend/db/view_count.py
+++ src/backend/db/view_count.py
@@ -1,5 +1,9 @@
 import re
-from pydantic import BaseModel, validator
+from zoneinfo import ZoneInfo
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, Field, validator, field_serializer
 
 from db import common
 
@@ -7,12 +11,19 @@ from db import common
 class Count(BaseModel):
     count: int
     video_id: str
+    date: Optional[datetime] = Field(default_factory=datetime.utcnow)
+    jst_date: Optional[datetime] = Field(default=datetime.now(tz=ZoneInfo("Asia/Tokyo")))
+    timestamp: Optional[int]
 
     @validator("video_id")
     def __post_init__(cls, v):
         if re.fullmatch(r"[a-zA-Z0-9_-]{11}", v) is None:
             raise ValueError("Invalid video id.")
         return v
+
+    @field_serializer("date", "jst_date")
+    def serialize_isofmt(self, time: datetime):
+        return time.isoformat()
 
 
 class _ViewCounter:
