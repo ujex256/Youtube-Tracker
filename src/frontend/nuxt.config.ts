86880705d1f5a2efa5f2f8f5a53df58cc04469df
fffena <132272963+fffena@users.noreply.github.com>
2024-04-02 10:29:49 +0000

    deps: Bootstrap v5を追加

--- src/frontend/nuxt.config.ts
+++ src/frontend/nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  srcDir: "./src"
+  srcDir: "./src",
+  modules: ["@bootstrap-vue-next/nuxt"],
+  css: [
+    "bootstrap/dist/css/bootstrap.min.css",
+  ],
 })
