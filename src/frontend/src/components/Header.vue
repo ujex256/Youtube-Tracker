bbabf4b8045ee3b597e3bb03bcbbf909c064c214
ujex256 <105550500+ujex256@users.noreply.github.com>
2024-04-11 18:58:27 +0900

    feat: header

--- src/frontend/src/components/Header.vue
+++ src/frontend/src/components/Header.vue
@@ -1,21 +1,33 @@
 <template>
   <!-- <div class="btn">Testing</div> -->
   <header data-bs-theme="dark">
-    <BNavbar class="bg-secondary p-3">
-      <h1 class="navbar-brand">SNAME</h1>
+    <BNavbar class="hd-nav bg-secondary p-3" container="xl">
+      <h1 class="navbar-brand">
+        SNAME
+      </h1>
       <div class="btnss">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <a class="nav-link" href="#">Main</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Main2</a>
+            <a class="nav-link" href="#">Mainああああああああ2</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Main3</a>
           </li>
+          <a class="nav-item" href="#">
+            <BButton>Login</BButton>
+          </a>
         </ul>
       </div>
     </BNavbar>
   </header>
 </template>
+
+<style scoped>
+.hd-nav {
+  padding-left: 5%;
+  padding-right: 5%;
+}
+</style>
