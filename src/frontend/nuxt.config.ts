bd331a66c103d45d5e810e5118797ecb1a4590a4
ujex256 <105550500+ujex256@users.noreply.github.com>
2024-04-11 17:57:21 +0900

    chore: configのインデントを4に

--- src/frontend/nuxt.config.ts
+++ src/frontend/nuxt.config.ts
@@ -1,12 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  srcDir: "./src",
-  modules: [
-    "@bootstrap-vue-next/nuxt",
-    ["@nuxtjs/eslint-module", { lintOnStart: false }]
-  ],
-  css: [
-    "bootstrap/dist/css/bootstrap.min.css",
-  ],
-})
+    devtools: { enabled: true },
+    srcDir: "./src",
+    modules: [
+        "@bootstrap-vue-next/nuxt",
+        ["@nuxtjs/eslint-module", { lintOnStart: false }],
+    ],
+    css: ["bootstrap/dist/css/bootstrap.min.css"],
+});
