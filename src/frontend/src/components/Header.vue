70a0980c883e2830ccade2d80493bf030943a0b5
ujex256 <105550500+ujex256@users.noreply.github.com>
2024-04-12 22:50:10 +0900

    feat: 適当なヘッダー

--- src/frontend/src/components/Header.vue
+++ src/frontend/src/components/Header.vue
@@ -1,23 +1,23 @@
 <template>
   <!-- <div class="btn">Testing</div> -->
   <header data-bs-theme="dark">
-    <BNavbar class="hd-nav bg-secondary p-3" container="xl">
-      <h1 class="navbar-brand">
+    <BNavbar class="hd-nav shadow p-3" container="xl" sha>
+      <span class="navbar-brand mb-0 h1" style="font-size: 1.5rem;">
         SNAME
-      </h1>
-      <div class="btnss">
+      </span>
+      <div class="hd-btns">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link" href="#">Main</a>
+            <a class="nav-link" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Mainああああああああ2</a>
+            <a class="nav-link" href="#">About</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Main3</a>
           </li>
           <a class="nav-item" href="#">
-            <BButton>Login</BButton>
+            <BButton variant="outline-primary">Login</BButton>
           </a>
         </ul>
       </div>
@@ -29,5 +29,6 @@
 .hd-nav {
   padding-left: 5%;
   padding-right: 5%;
+  background-color: rgb(137, 195, 235);
 }
 </style>
