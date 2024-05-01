5a9309b09660fc08c6dd621f0abe30d7c1e4c041
ujex256 <105550500+ujex256@users.noreply.github.com>
2024-05-01 20:39:29 +0900

    wip

--- src/frontend/src/pages/index.vue
+++ src/frontend/src/pages/index.vue
@@ -1,7 +1,27 @@
 <template>
   <div>
-    <div class="pg-info">
-      <h1>Hello, World</h1>
+    <div class="pg-info text-center">
+      <div class="info-txt">
+        <h1 class="info-catchphrase">Hello, World</h1>
+        <span>
+          Youtubeなどのプラットフォームの動画を分析
+        </span>
+      </div>
+      <img src="https://placehold.jp/500x350.png" alt="すごい紹介">
     </div>
   </div>
 </template>
+
+<style scoped>
+.pg-info {
+  height: 75vh;
+  background-color: wheat;
+}
+.info-txt {
+  padding-top: 5vh;
+  font-size: 30px;
+}
+.info-catchphrase {
+  font-size: 50px;
+}
+</style>
