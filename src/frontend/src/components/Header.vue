480836c2444d3d92d9f5045c7e5c4f8134a54baf
ujex256 <105550500+ujex256@users.noreply.github.com>
2024-05-01 19:55:02 +0900

    fix: marginが変になっていた

--- src/frontend/src/components/Header.vue
+++ src/frontend/src/components/Header.vue
@@ -5,7 +5,7 @@
         SNAME
       </span>
       <div class="hd-btns">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <ul class="navbar-nav me-auto mb-lg-0">
           <li class="nav-item">
             <a class="nav-link" href="#">Home</a>
           </li>
