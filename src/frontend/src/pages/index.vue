48bca3776b109b27221feb08423f1d6ec32555cb
ujex256 <105550500+ujex256@users.noreply.github.com>
2024-05-06 11:29:15 +0900

    feat: gridを使ってレスポンシブに対応

--- src/frontend/src/pages/index.vue
+++ src/frontend/src/pages/index.vue
@@ -1,25 +1,33 @@
 <template>
   <div>
     <div class="pg-info">
-      <div class="info-txt">
-        <h1 class="info-catchphrase">Hello, World</h1>
-        <span>Youtube, NicoNicoなど</span>
-        <br />
-        <span>様々なプラットフォームの動画の</span>
-        <br />
-        <span>再生回数や高評価数などを分析します。</span>
-        <div class="d-grid gap-2">
-          <BButton pill variant="primary" class="registerbtn m-3 px-5">
-            <Icon name="ant-design:video-camera-add-outlined" size="1.5em" />
-            自分の動画を登録してみる
-          </BButton>
-        </div>
-      </div>
-      <img
-        src="https://placehold.jp/500x350.png"
-        alt="すごい紹介"
-        class="info-img"
-      />
+      <BContainer>
+        <BRow align-content="center">
+          <BCol lg="6" align-self="center">
+            <div class="info-txt">
+              <h1 class="info-catchphrase">Hello, World</h1>
+              <span>Youtube, NicoNicoなど</span>
+              <br />
+              <span>様々なプラットフォームの動画の</span>
+              <br />
+              <span>再生回数や高評価数などを分析します。</span>
+              <div class="d-grid gap-2">
+                <BButton pill variant="primary" class="registerbtn m-3 px-5">
+                  <Icon name="ant-design:video-camera-add-outlined" size="1.5em" />
+                  自分の動画を登録してみる
+                </BButton>
+              </div>
+            </div>
+          </BCol>
+          <BCol>
+            <img
+              src="https://placehold.jp/500x350.png"
+              alt="すごい紹介"
+              class="info-img"
+            />
+          </BCol>
+        </BRow>
+      </BContainer>
     </div>
   </div>
 </template>
@@ -38,7 +46,6 @@
   max-width: 50vw;
   padding: 25px;
   width: fit-content;
-  height: 75%;
   font-size: 1.25rem;
 }
 .info-catchphrase {
