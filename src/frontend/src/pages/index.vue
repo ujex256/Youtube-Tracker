7fa52d0a21b8279236b236b81ba40c0689cb10a0
ujex256 <105550500+ujex256@users.noreply.github.com>
2024-05-06 11:14:34 +0900

    lint

--- src/frontend/src/pages/index.vue
+++ src/frontend/src/pages/index.vue
@@ -3,28 +3,23 @@
     <div class="pg-info">
       <div class="info-txt">
         <h1 class="info-catchphrase">Hello, World</h1>
-        <span>
-          Youtube, NicoNicoなど
-        </span>
-        <br>
-        <span>
-          様々なプラットフォームの動画の
-        </span>
-        <br>
-        <span>
-          再生回数や高評価数などを分析します。
-        </span>
-        <BButton
-          pill
-          block
-          variant="primary"
-          class="registerbtn m-3 px-5"
-        >
-          <Icon name="ant-design:video-camera-add-outlined" size="1.5em" />
-          自分の動画を登録してみる
-        </BButton>
+        <span>Youtube, NicoNicoなど</span>
+        <br />
+        <span>様々なプラットフォームの動画の</span>
+        <br />
+        <span>再生回数や高評価数などを分析します。</span>
+        <div class="d-grid gap-2">
+          <BButton pill variant="primary" class="registerbtn m-3 px-5">
+            <Icon name="ant-design:video-camera-add-outlined" size="1.5em" />
+            自分の動画を登録してみる
+          </BButton>
+        </div>
       </div>
-      <img src="https://placehold.jp/500x350.png" alt="すごい紹介" class="info-img">
+      <img
+        src="https://placehold.jp/500x350.png"
+        alt="すごい紹介"
+        class="info-img"
+      />
     </div>
   </div>
 </template>
@@ -40,8 +35,8 @@
 .info-txt {
   position: relative;
   word-break: keep-all;
-  max-width: 40vw;
-  padding: 20px;
+  max-width: 50vw;
+  padding: 25px;
   width: fit-content;
   height: 75%;
   font-size: 1.25rem;
@@ -54,7 +49,6 @@
   height: auto;
 }
 .registerbtn {
-  /* width: 100%; */
   padding-top: 0.75rem;
   padding-bottom: 0.75rem;
 }
