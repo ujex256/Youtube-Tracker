93da8cb8961028f4c46674ed819d2ddf094b8705
ujex256 <105550500+ujex256@users.noreply.github.com>
2024-05-30 15:02:56 +0900

    add: Iconを含んだdropdownのitem

---
+++ src/frontend/src/components/DropIconItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <BDropdownItem v-bind="bProps">
+    <Icon :name="props.name" />
+    <slot />
+  </BDropdownItem>
+</template>
+
+<script setup lang="ts">
+import { type BDropdownItemProps } from "bootstrap-vue-next";
+
+interface propsTypes {
+  name: string;
+}
+interface DropIconItemProps extends propsTypes, BDropdownItemProps {}
+
+const props = defineProps<DropIconItemProps>();
+const bProps = Object.fromEntries(
+  Object.entries(props).filter(([key]) => !Object.keys(props as propsTypes).includes(key))
+) as BDropdownItemProps;
+</script>
